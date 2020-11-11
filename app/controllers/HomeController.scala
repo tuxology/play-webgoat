be57e9b81c247cf18f7c4074f14fe6d946fda97b
Suchakra Sharma <suchakra@gmail.com>
2020-11-11 07:51:18 -0800

    Remove some RCEs

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -44,8 +44,6 @@ class HomeController @Inject()(ws: WSClient, cc: MessagesControllerComponents)(i
   def attackerQuerySimple = Action { implicit request  =>
     val address = request.getQueryString("address")
 
-    // [RuleTest] Command Injection 
-    s"ping ${address}".!
 
     // [RuleTest] Cross-Site Scripting: Reflected
     val html = Html(s"Host ${address} pinged")
@@ -61,10 +59,7 @@ class HomeController @Inject()(ws: WSClient, cc: MessagesControllerComponents)(i
     val addressRE= "(.*):(\\d+)".r
     val address = request.cookies.get("address").get.value
 
-    address match {
-      // [RuleTest] Command Injection 
-      case addressRE(address, port) => s"ping ${address}".!
-    }
+
     // [RuleTest] Cross-Site Scripting: Reflected
     Ok(Html(s"Host ${address} pinged")) as HTML
   }
